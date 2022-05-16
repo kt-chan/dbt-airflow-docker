84deff726223c20fc0cce13502712727ac4f0c0a
kt-chan <ktchan@gmail.com>
2022-05-16 11:36:30 +0800

    setup

--- cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-chmod +x cleanup.sh
-chmod 777 -R airflow
-docker-compose down --volumes --rmi all
--- scripts_airflow/init.sh
+++ scripts_airflow/init.sh
@@ -9,12 +9,11 @@ export AIRFLOW__WEBSERVER__SECRET_KEY
 
 DBT_POSTGRESQL_CONN="postgresql+psycopg2://${DBT_POSTGRES_USER}:${DBT_POSTGRES_PASSWORD}@${DBT_POSTGRES_HOST}:${POSTGRES_PORT}/${DBT_POSTGRES_DB}"
 
-cd /dbt && dbt compile
-# rm -f /opt/airflow/airflow-webserver.pid
+# cd /dbt && rm -rf ./target/* && dbt compile
+# rm -f /airflow/airflow-webserver.pid
 
 sleep 10
-airflow resetdb 
-
+airflow initdb
 sleep 10
 airflow connections --add --conn_id 'dbt_postgres_instance_raw_data' --conn_uri $DBT_POSTGRESQL_CONN
 airflow scheduler & airflow webserver
