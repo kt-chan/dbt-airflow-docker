a4a03530f323a6ae845e4d407ea387c006859e87
kt-chan <ktchan@gmail.com>
2022-05-18 11:54:06 +0800

    set for dbt folder update

--- clean.sh
+++ clean.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-rm -rf ./airflow/*
-mkdir ./airflow/dags
-mkdir ./airflow/logs
-mkdir ./airflow/plugins
-
 #docker-compose down --rmi all --volumes 
 docker-compose down
-docker volume rm $(docker volume ls -q)
+
+if [[ -n $(docker volume ls -q) ]]; then
+	docker volume rm $(docker volume ls -q)
+fi
 
 
 
--- scripts/dbt_init.sh
+++ scripts/dbt_init.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
 
-cd /dbt && rm -rf ./target/* && dbt compile
-dbt docs generate && dbt docs serve --port 8081
+cd /dbt 
+rm -rf ./target/* 
+dbt compile
+dbt docs generate 
+dbt docs serve --port 8081 & 
+dbt-rpc serve --port 8580 & 
+
+# Wait for any process to exit
+wait -n
+  
+# Exit with status of process that exited first
+exit $?
