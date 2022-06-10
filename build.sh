486944b9e6dfb1b93249ad39a7cdc46d001b7551
kt-chan <ktchan@gmail.com>
2022-06-10 14:51:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ if [[ (! -z "$1") &&  ("$1" == "force") ]]; then
 	docker build -f ./dockerfile/dockerfile_airflow -t airflow:2.3.0 -t airflow:latest .
 	docker build -f ./dockerfile/dockerfile_hadoop -t hadoop-service:3.2.3 -t hadoop-service:latest .
 	docker build -f ./dockerfile/dockerfile_jupyter -t jupyter-spark:3.2.1 -t jupyter-spark:latest .
-	docker build -f ./dockerfile/dockerfile_sparkbase -t sparkbase:3.2.1 -t sparkbase:latest .
+	docker build -f ./dockerfile/dockerfile_sparkbase -t spark-base:3.2.1 -t spark-base:latest .
 	docker build -f ./dockerfile/dockerfile_sparkmaster-t spark-master:3.2.1 -t spark-master:latest .
-	docker build -f ./dockerfile/dockerfile_sparkworkder -t spark-workder:3.2.1 -t spark-workder:latest .
+	docker build -f ./dockerfile/dockerfile_sparkworker -t spark-worker:3.2.1 -t spark-worker:latest .
 	
 else
 
