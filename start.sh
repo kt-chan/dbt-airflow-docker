0011ef02ca856ecbeb6d63943608a0b180496fb5
kt-chan <ktchan@gmail.com>
2022-06-14 18:36:16 +0800

    update script and dbt profile

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 docker-compose build
-docker-compose up hadoop-service -d
-docker-compose up spark-master -d
+docker-compose up dbt-service -d
 docker-compose up airflow-init -d
 docker-compose up -d
 
