a7c7046a94749f64b8c3463795ac92409d9bc6c3
kt-chan <ktchan@gmail.com>
2022-06-16 12:10:59 +0800

    stable run v1

--- scripts/runHadoop.sh
+++ scripts/runHadoop.sh
@@ -9,10 +9,6 @@ $HADOOP_HOME/bin/hdfs namenode -format -force
 #         $HADOOP_HOME/bin/hdfs namenode -format -force
 # fi
 
-sudo mkdir -p $HADOOP_HOME/logs
-sudo chown hdfs:hdfs $HADOOP_HOME/logs
-sudo chmod 755 $HADOOP_HOME/logs
-
 cd $HADOOP_HOME
 sed -i -e "s/localhost/$HOSTNAME/g"  $HADOOP_HOME/etc/hadoop/core-site.xml
 ./sbin/start-dfs.sh
