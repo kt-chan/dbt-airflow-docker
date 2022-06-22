ae7ca78d415fe60a113bca0f4221e15405260017
kt-chan <ktchan@gmail.com>
2022-06-22 10:34:00 +0800

    update user and log directory setting

--- scripts/runMaster.sh
+++ scripts/runMaster.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ${SPARK_HOME}
-./bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> ./logs/spark-master.out &
+./bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> /tmp/logs/spark-master.out &
 
 
 tail -f /dev/null
--- scripts/runWorker.sh
+++ scripts/runWorker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd ${SPARK_HOME}
-./bin/spark-class org.apache.spark.deploy.worker.Worker spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT} >> ./logs/spark-worker.out
+./bin/spark-class org.apache.spark.deploy.worker.Worker spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT} >> /tmp/logs/spark-worker.out
 
 tail -f /dev/null
