2784721826a893500387453ea4bac28477f6c991
kt-chan <ktchan@gmail.com>
2022-06-22 11:59:34 +0800

    set up for hudi v0.1

--- scripts/runThrift.sh
+++ scripts/runThrift.sh
@@ -2,16 +2,18 @@
 
 cd ${SPARK_HOME}
 
-# Spark 3.2 standard 
-./sbin/start-thriftserver.sh --master spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT >> /tmp/logs/start-thriftserver.out & 
+## # Spark 3.2 standard 
+## ./sbin/start-thriftserver.sh --master spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT >> /tmp/logs/start-thriftserver.out & 
 
-## # Spark 3.2 with hudi table [this does not work for spark thrift sql]
-## ./bin/spark-submit --master spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT \
-## --packages org.apache.hudi:hudi-spark3.2-bundle_2.12:0.11.0 \
-## --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 \
+# Spark 3.2 with hudi table [this does not work for spark thrift sql]
+## # Configuration done in spark-defaults.xml
 ## --conf 'spark.serializer=org.apache.spark.serializer.KryoSerializer' \
 ## --conf 'spark.sql.extensions=org.apache.spark.sql.hudi.HoodieSparkSessionExtension' \
-## --conf 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.hudi.catalog.HoodieCatalog'
+## --conf 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.hudi.catalog.HoodieCatalog' \
 
+./bin/spark-submit --master spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT \
+--packages org.apache.hudi:hudi-spark3-bundle_2.12:0.11.1,org.apache.spark:spark-avro_2.12:3.1.2 \
+--class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 \
+>> /tmp/logs/start-thriftserver.out & 
 
 tail -f /tmp/logs/start-thriftserver.out
